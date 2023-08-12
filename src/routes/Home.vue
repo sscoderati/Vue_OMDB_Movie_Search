e77a554840c8ac741fe9a09288b9c9d6ace04917
Cheshier <chungup5495@gmail.com>
2023-08-13 01:01:42 +0900

    Feat: 메인 화면에 컴포넌트 등록

---
+++ src/routes/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Search from "../components/Search.vue";
+import MovieList from "../components/MovieList.vue";
+import AddMovies from "../components/AddMovies.vue";
+</script>
+
+<template>
+  <Search />
+  <MovieList />
+  <AddMovies />
+</template>
+
+<style scoped></style>
