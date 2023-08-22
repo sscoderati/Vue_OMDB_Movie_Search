e0468c43ebee7daded68859be5c0d58c5b8c5394
Cheshier <chungup5495@gmail.com>
2023-08-22 15:43:43 +0900

    Refactor: 영화 상세 정보 페이지에서 뒤로가기 시 스크롤 위치 유지

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,32 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "./Home.vue";
+import {
+  createRouter,
+  createWebHistory,
+  RouteLocationNormalized,
+  RouteLocationNormalizedLoaded,
+} from "vue-router";
+import MainView from "./MainView.vue";
 import MovieDetail from "./MovieDetail.vue";
 import NotFound from "./NotFound.vue";
 
 export default createRouter({
   history: createWebHistory(),
-  scrollBehavior: () => ({ top: 0 }),
+  scrollBehavior: (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalizedLoaded,
+    savedPosition: NonNullable<unknown> | null,
+  ) => {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.path === "/" && from.path.startsWith("/movie")) {
+      return false;
+    } else {
+      return { top: 0 };
+    }
+  },
   routes: [
     {
       path: "/",
-      component: Home,
+      component: MainView,
     },
     {
       path: "/movie/:id",
