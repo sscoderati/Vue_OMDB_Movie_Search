aeb2ed5442b1f9159e3a17c8a67a001d2d68b0bc
Cheshier <chungup5495@gmail.com>
2023-08-22 17:07:25 +0900

    Refactor: 더보기 버튼 가시 조건 함수화, 클래스명 및 상수명 변경
    isMovieLeft -> hasNextPage
    add-more-movies -> show-more-movies

--- src/components/AddMovies.vue
+++ src/components/AddMovies.vue
@@ -2,34 +2,36 @@
 import { useMovieStore } from "../store/movie.ts";
 import { ref, watch } from "vue";
 const movieStore = useMovieStore();
-const isMovieLeft = ref(true);
-
+const hasNextPage = ref(true);
+const isShowMoreBtnVisible = () => {
+  return (
+    hasNextPage.value &&
+    movieStore.maxPage &&
+    !movieStore.isLoading &&
+    movieStore.isNormal === "True"
+  );
+};
 watch(
   () => movieStore.page,
   () => {
     const { page, maxPage } = movieStore;
-    isMovieLeft.value = maxPage > page;
+    hasNextPage.value = maxPage > page;
   },
 );
 </script>
 
 <template>
   <button
-    v-if="
-      isMovieLeft &&
-      movieStore.maxPage &&
-      !movieStore.isLoading &&
-      movieStore.isNormal === 'True'
-    "
-    class="btn add-more-movies"
     @click="movieStore.searchMovies(movieStore.page + 1)"
+    v-if="isShowMoreBtnVisible()"
+    class="btn show-more-movies"
   >
     더 보기!
   </button>
 </template>
 
 <style scoped lang="scss">
-.add-more-movies {
+.show-more-movies {
   width: 100%;
   margin: 20px auto;
   display: block;
